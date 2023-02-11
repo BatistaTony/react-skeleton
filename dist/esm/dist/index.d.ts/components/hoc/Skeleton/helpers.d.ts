6d9c001ff63e8d6db3ff502234acfce6099e9a78
Batista Oliveira <batistatomeoliveira96@gmail.com>
2023-02-11 16:15:48 +0100

    ref: remove dist folder

--- dist/esm/dist/index.d.ts/components/hoc/Skeleton/helpers.d.ts
+++
@@ -1,8 +0,0 @@
-export interface addClassesProps {
-    container: HTMLDivElement;
-    skeletonFirstColor?: string;
-    skeletonSecondColor?: string;
-}
-export declare const clearValues: <T>(data: T) => T;
-export declare const addClasses: ({ container, skeletonFirstColor, skeletonSecondColor, }: addClassesProps) => void;
-export declare const removeClasses: () => void;
--- dist/esm/dist/index.d.ts/components/hoc/Skeleton/skeleton.d.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentType } from "react";
-import "./../../../styles/styles.css";
-export type WrappedProps = {
-    isLoading: boolean;
-    skeletonFirstColor?: string;
-    skeletonSecondColor?: string;
-};
-declare const Skeleton: <T extends object>(WrappedComponent: ComponentType<T>) => (props: T & WrappedProps) => JSX.Element;
-export default Skeleton;
